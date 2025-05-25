77d228bf7ff45fd5cd8618e5a8b896b3e2aeeed4
Trevor Rowbotham <trevor.rowbotham@pm.me>
2025-05-25 17:08:39 -0400

    Pass through codepoints unmodified

--- src/CodePoint.php
+++ src/CodePoint.php
@@ -48,90 +48,36 @@ final class CodePoint
     }
 
     /**
-     * Takes a UTF-8 encoded string and converts it into a series of integer code points. Any
-     * invalid byte sequences will be replaced by a U+FFFD replacement code point.
-     *
-     * @see https://encoding.spec.whatwg.org/#utf-8-decoder
-     *
      * @return list<int>
      */
     public static function utf8Decode(string $input): array
     {
-        $bytesSeen = 0;
-        $bytesNeeded = 0;
-        $lowerBoundary = 0x80;
-        $upperBoundary = 0xBF;
-        $codePoint = 0;
         $codePoints = [];
-        $length = strlen($input);
+        $shifts = [
+            ['byte' => [], 'shifts' => []],
+            ['byte' => [0xC0], 'shifts' => [6]],
+            ['byte' => [0xE0, 0x80], 'shifts' => [12, 6]],
+            ['byte' => [0xF0, 0x80, 0x80], 'shifts' => [18, 12, 6]],
+        ];
 
-        for ($i = 0; $i < $length; ++$i) {
-            $byte = ord($input[$i]);
+        foreach (mb_str_split($input, 1, 'utf-8') as $s) {
+            $bytes = strlen($s);
 
-            if ($bytesNeeded === 0) {
-                if ($byte <= 0x7F) {
-                    $codePoints[] = $byte;
-
-                    continue;
-                }
-
-                if ($byte >= 0xC2 && $byte <= 0xDF) {
-                    $bytesNeeded = 1;
-                    $codePoint = $byte & 0x1F;
-                } elseif ($byte >= 0xE0 && $byte <= 0xEF) {
-                    if ($byte === 0xE0) {
-                        $lowerBoundary = 0xA0;
-                    } elseif ($byte === 0xED) {
-                        $upperBoundary = 0x9F;
-                    }
-
-                    $bytesNeeded = 2;
-                    $codePoint = $byte & 0xF;
-                } elseif ($byte >= 0xF0 && $byte <= 0xF4) {
-                    if ($byte === 0xF0) {
-                        $lowerBoundary = 0x90;
-                    } elseif ($byte === 0xF4) {
-                        $upperBoundary = 0x8F;
-                    }
-
-                    $bytesNeeded = 3;
-                    $codePoint = $byte & 0x7;
-                } else {
-                    $codePoints[] = 0xFFFD;
-                }
+            if ($bytes === 1) {
+                $codePoints[] = ord($s);
 
                 continue;
             }
 
-            if ($byte < $lowerBoundary || $byte > $upperBoundary) {
-                $codePoint = 0;
-                $bytesNeeded = 0;
-                $bytesSeen = 0;
-                $lowerBoundary = 0x80;
-                $upperBoundary = 0xBF;
-                --$i;
-                $codePoints[] = 0xFFFD;
+            $bytesLength = $bytes - 1;
+            $x = 0;
 
-                continue;
+            for ($i = 0; $i < $bytesLength; ++$i) {
+                $x += (ord($s[$i]) - $shifts[$bytesLength]['byte'][$i]) << $shifts[$bytesLength]['shifts'][$i];
             }
 
-            $lowerBoundary = 0x80;
-            $upperBoundary = 0xBF;
-            $codePoint = ($codePoint << 6) | ($byte & 0x3F);
-
-            if (++$bytesSeen !== $bytesNeeded) {
-                continue;
-            }
-
-            $codePoints[] = $codePoint;
-            $codePoint = 0;
-            $bytesNeeded = 0;
-            $bytesSeen = 0;
-        }
-
-        // String unexpectedly ended, so append a U+FFFD code point.
-        if ($bytesNeeded !== 0) {
-            $codePoints[] = 0xFFFD;
+            $x += ord($s[$bytesLength]) - 0x80;
+            $codePoints[] = $x;
         }
 
         return $codePoints;
